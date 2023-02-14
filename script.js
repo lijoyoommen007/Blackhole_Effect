1fe2eb412e74fcc9d55029bc0d91a9cacdf28244
Lijoy <oommenlijoy@gmail.com>
2023-02-14 19:52:07 +0530

    first commit

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function animate() {
   context.clearRect(0, 0, canvas.width, canvas.height);
 
   // draw lines for the outer pattern
-  for (let i = 0; i < 30; i++) {
-    let angle = (i / 30) * Math.PI * 2;
+  for (let i = 0; i < 20; i++) {
+    let angle = (i / 20) * Math.PI * 2;
     let x1 = Math.cos(angle) * (canvas.width / 2 - radius);
     let y1 = Math.sin(angle) * (canvas.height / 2 - radius);
     let x2 = Math.cos(angle) * (radius);
@@ -28,8 +28,8 @@ function animate() {
   }
 
   // draw lines for the inner pattern
-  for (let i = 0; i < 30; i++) {
-    let angle = (i / 30) * Math.PI * 2;
+  for (let i = 0; i < 20; i++) {
+    let angle = (i / 20) * Math.PI * 2;
     let x1 = Math.cos(angle) * (radius / 2);
     let y1 = Math.sin(angle) * (radius / 2);
     let x2 = Math.cos(angle) * (radius);
@@ -43,17 +43,35 @@ function animate() {
     context.stroke();
   }
 
-    // draw filled circle
-    let radius2 =10
+  // draw filled circle
+  // context.beginPath();
+  // context.arc(canvas.width / 2, canvas.height / 2, radius / 2, 0, Math.PI * 2);
+  // context.fillStyle = 'white';
+  // context.fill();
+
+  // draw  pattern inside the circle
+
+  for (let i = 0; i < 20; i++) {
+    let angle = (i / 20) * Math.PI * 2;
+    let x1 = Math.cos(angle) * (radius /6 );
+    let y1 = Math.sin(angle) * (radius / 6);
+    let x2 = Math.cos(angle) * (radius);
+    let y2 = Math.sin(angle) * (radius);
+
     context.beginPath();
-    context.arc(canvas.width / 2, canvas.height / 2, 10 / 4, 0, Math.PI * 2);
-    context.fillStyle = 'white';
-    context.fill();
+    context.moveTo(canvas.width / 2 + x1, canvas.height / 2 + y1);
+    context.lineTo(canvas.width / 2 + x2, canvas.height / 2 + y2);
+    context.strokeStyle = "white";
+    context.lineWidth = 2;
+    context.stroke();
+  }
 
   // increase radius
-  radius += 0.8;
-  if (radius > maxRadius + 10) {
-    radius = 10;
+  radius += 0.5;
+
+  // reset radius if it's greater than maxRadius
+  if (radius > maxRadius) {
+    radius = 0;
   }
 }
 
